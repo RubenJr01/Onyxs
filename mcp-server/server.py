ed675d1cc1bd2c76c0d01facb50fceaa8750aa02
RubenJr01 <ruben.delacruzjr01@gmail.com>
2025-11-01 19:12:50 -0500

    Implement USDA food search API endpoint with FastAPI

--- mcp-server/server.py
+++ mcp-server/server.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import httpx
+import os
 
 app = FastAPI()
+load_dotenv()
 
 @app.get("/")
-
 def read_root():
     return {"Message": "Onyxs Fitness API is running!"}
 
-def search_food()
+@app.post("/search_food")
+async def search_food(query: str, limit: int = 15):
+    API_KEY = os.getenv("USDA_API_KEY")
+    url = "https://api.nal.usda.gov/fdc/v1/foods/search"
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, params= {
+            "api_key": API_KEY,
+            "query": query,
+            "pageSize": limit
+        })
+    return response.json()
